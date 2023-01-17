66a4dd68b02a614ada8367bbb63aeb9decb126ae
nepalsandesh <sandeshnepal43@gmail.com>
2023-01-17 21:27:33 +0545

    Authentication, now only authenticated users can chat

--- core/asgi.py
+++ core/asgi.py
@@ -13,12 +13,11 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+from channels.auth import AuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
-  'websocket': URLRouter(
-    chat.routing.websocket_urlpatterns
-  ),
+  'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
 })
